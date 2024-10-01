4760a4a7a98c4ebdf25631012c4346eeca05096c
Smriti <Smriti.Smriti@nspower.ca>
2024-10-01 11:34:07 +0530

    main

---
+++ main.tf
@@ -0,0 +1,27 @@
+# Configure the Azure provider
+terraform {
+  cloud {
+   organization = "Azure_Labs"
+
+   workspaces {
+     name = "Demo"
+   }
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "Central India"
+}
